b268de0511fed4360715f4a571faf694d8f63dea
mapofthemoon <90924243+mapofthemoon@users.noreply.github.com>
2023-03-26 22:04:53 +0600

    Delete lab7/functions directory

--- lab7/functions/b (2).py
+++
@@ -1,4 +0,0 @@
-def pow(a,p):
-    print(a**p)
-a, p= list(map(float,input().split()))
-pow(a,p)
--- lab7/functions/c (2).py
+++
@@ -1,6 +0,0 @@
-def xor(x,y):
-    if x + y ==2 or x+y==0:
-        print(0)
-    else: print(1)
-a, p= list(map(float,input().split()))
-xor(a,p)
